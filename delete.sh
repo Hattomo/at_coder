20f14aea7eef4f0b6562bbb31ee7229f6fd70180
Hattomo <45988993+Hattomo@users.noreply.github.com>
2020-06-03 21:28:28 +0900

    add deleting exec files

--- delete.sh
+++ delete.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 find . -not -path "*/.git/*" -type f ! -name "*.*"
+find . \( -name "a.out" -or -name "a.exe" \) 
 find . -not -path "*/.git/*" -type f ! -name "*.*"  | xargs rm -rf
+find . \( -name "a.out" -or -name "a.exe" \) | xargs rm -rf
