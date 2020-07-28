7961831ac53f61fcb4f0b7bf8c068c50c929b097
Hattomo <45988993+Hattomo@users.noreply.github.com>
2020-07-28 22:40:07 +0900

    practice

---
+++ abc/154/C.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <iomanip>
+#include <stdio.h>
+#include <algorithm>
+#include <ctime>
+#include <vector>
+#include <set>
+#include <queue>
+using namespace std;
+typedef long long ll;
+
+#define PI 3.14159265358979323846264338327950L
+
+string res;
+
+int main()
+{
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+    }
+    res = "YES";
+    sort(A.begin(), A.end());
+    for (int i = 0; i < N - 1; i++)
+    {
+        if (A[i] == A[i + 1])
+        {
+            res = "NO";
+        }
+    }
+    cout << res << endl;
+    return 0;
+}
